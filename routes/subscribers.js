fa8e19a06b19a9e08fd14323430b55415dec62ed
ALEKSEY0305 <alex.romanovich.ak@gmail.com>
2023-01-24 05:52:38 +0900

    set up packages and establishing local host

---
+++ routes/subscribers.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+
+// Getting all
+router.get('/', (req, res) => {
+    res.send('Hello world')
+})
+// Getting One
+router.get('/:id', (req, res) => {
+    // req.params.id
+})
+// Creating one
+router.post('/', (req, res) => {
+
+})
+// Updating one
+router.patch('/', (req, res) => {
+
+})
+// Deleting one
+router.delete('/:id', (req, res) => {
+
+})
+
+
+
+module.exports = router
